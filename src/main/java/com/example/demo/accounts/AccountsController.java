a97b68d74dec043c1964b075c7178e3d201361d6
Lindy Lim <lindylimlw@gmail.com>
2020-10-29 08:37:30 +0800

    Completed AccountsController.java

--- src/main/java/com/example/demo/accounts/AccountsController.java
+++ src/main/java/com/example/demo/accounts/AccountsController.java
@@ -1,6 +1,11 @@
 package com.example.demo.accounts;
 
-import org.springframework.beans.factory.annotation.Autowired;  
+import java.util.Iterator;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.web.bind.annotation.GetMapping; 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,13 +23,60 @@ public class AccountsController {
     @Autowired
     private AccountsRepository accRepository;
 
-    User user = new User();
-    public AccountsController() {
-        // this.user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    public AccountsController() {}
+
+    User currentUser;
+    
+    public void authenticateCurrentUser() {
+        AuthorizedUser context = new AuthorizedUser();
+        this.currentUser = context.getUser();
     }
 
     @PostMapping(path="/accounts")
     public @ResponseBody String addAccount (@RequestBody Account account) {
-        return "";
+        authenticateCurrentUser();
+
+        accRepository.save(account);
+
+        return "Account saved!\n" + account.toString();
+    }
+
+    @GetMapping(path="/accounts")
+    public @ResponseBody Iterable<Account> getAccounts() {
+        authenticateCurrentUser();
+
+        int userID = currentUser.getId();
+        
+        Iterable<Account> accounts = accRepository.findAll();
+        Iterator<Account> iter = accounts.iterator();
+
+        while(iter.hasNext()) {
+            Account acc = iter.next();
+            if(acc.getCustomer_id() != userID) {
+                iter.remove();
+            }
+        }
+        return accounts;
+    }
+
+    @GetMapping(path="/accounts/{id}")
+    public @ResponseBody Account getAccountById(@PathVariable int id) {
+        authenticateCurrentUser();
+
+        int userID = currentUser.getId();
+
+        Optional<Account> accountEntity = accRepository.findById(id);
+        Account account;
+        if (!accountEntity.isPresent()) {
+            throw new AccountNotFoundException(id);
+        } else {
+            account = accountEntity.get();
+            if (account.getCustomer_id() != userID) {
+                System.out.println("No account of this ID associated with this user.");
+                throw new AccountNotFoundException(id);
+            }
+        }
+
+        return account;
     }
 }
