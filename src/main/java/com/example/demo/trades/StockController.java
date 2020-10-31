c8f01655a881dde12896c21f442ed563a1efca76
Lindy Lim <lindylimlw@gmail.com>
2020-11-01 01:11:30 +0800

    Added methods to StockController - stable build

--- src/main/java/com/example/demo/trades/StockController.java
+++ src/main/java/com/example/demo/trades/StockController.java
@@ -1,7 +1,16 @@
 package com.example.demo.trades;
 
 import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.web.bind.annotation.GetMapping; 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+import org.springframework.web.server.ResponseStatusException;
+
 
 @RestController
 public class StockController {
@@ -11,5 +20,17 @@ public class StockController {
     @Autowired
     public StockController(StocksRepository stocksRepository) {
         this.stocksRepository = stocksRepository;
+    }
+
+    @GetMapping(path="/stocks")
+    @ResponseStatus(HttpStatus.OK)
+    public @ResponseBody Iterable<Stock> getStocks() {
+        return null;
+    }
+
+    @GetMapping(path="/stocks/{symbol}")
+    @ResponseStatus(HttpStatus.OK)
+    public @ResponseBody Stock getStockById(@PathVariable String symbol) {
+        return null;
     }
 }
