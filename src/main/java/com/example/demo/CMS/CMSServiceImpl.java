b157d7466f759a03600c315a915e390afad1afd5
vrindabahl <vrinda.bahl.2019@sis.smu.edu.sg>
2020-11-01 22:27:49 +0530

    cms final

--- src/main/java/com/example/demo/CMS/CMSServiceImpl.java
+++ src/main/java/com/example/demo/CMS/CMSServiceImpl.java
@@ -4,74 +4,83 @@ import java.util.*;
 import org.springframework.stereotype.Service;
 
 
-// @Service
-// public class CMSServiceImpl implements CMSService {
+@Service
+public class CMSServiceImpl implements CMSService {
    
-//     private CMSRepository cms;
+    private CMSRepository cmsRepository;
     
 
-//     public CMSServiceImpl(CMSRepository cms){
-//         this.cms = cms;
-//     }
+    public CMSServiceImpl(CMSRepository cms){
+        this.cmsRepository = cms;
+    }
 
-//     @Override
-//     public List<Content> listContent() {
-//         List<Content> buffer = cms.findAll();
-//         for (int i =0; i< buffer.size(); i++){
-//             if (!buffer.get(i).getApproved()){
-//                 buffer.remove(i);
-//             }
-//         }
-//         return buffer;
-//     }
+    @Override
+    public List<Content> listContent() {
+        List<Content> buffer = cmsRepository.findAll();
+        for (int i =0; i< buffer.size(); i++){
+            if (!buffer.get(i).getApproved()){
+                buffer.remove(i);
+            }
+        }
+        return buffer;
+    }
 
-//     @Override
-//     public Content getContent(int id) {
-//         Optional<Content> contentEntity = cms.findById(id);
-//         Content content;
-//         if (!contentEntity.isPresent()) {
-//             throw new ContentNotFoundException(id);
-//         } else {
-//             content = contentEntity.get();
-//             if(!content.getApproved()){
-//                 return null;
-//             }
-//         }
-//         return content;
-//     }
+    @Override
+    public Content getContent(int id) {
+        Optional<Content> contentEntity = cmsRepository.findById(id);
+        Content content;
+        if (!contentEntity.isPresent()) {
+            throw new ContentNotFoundException(id);
+        } else {
+            content = contentEntity.get();
+            if(!content.getApproved()){
+                return null;
+            }
+        }
+        return content;
+    }
     
-//     @Override
-//     public Content addContent(Content content) {
-//         // Optional<Content> contentEntity = cms.findById(id);
-//         // if (!contentEntity.isPresent()) {
-//         //     throw new ContentNotFoundException(id);
-//         // } else {
-//             return cms.save(content);
-//         // }
-//     }
+    @Override
+    public Content addContent(Content content) {
+            return cmsRepository.save(content);
+    }
     
-//     @Override
-//     public Content updateContent(int id, Content newContentInfo){
-//             Optional<Content> c = cms.findById(id);
-//             if (c.isPresent()) {
-//                 Content content = c.get();
-//                 content.setTitle(newContentInfo.getTitle());
-//                 return cms.save(content);
-//             }
-//             else 
-//                 return null;
-//     }
+    @Override
+    public Content updateContent(int id, Content newContentInfo){
+            Optional<Content> c = cmsRepository.findById(id);
+            if (c.isPresent()) {
+                Content content = c.get();
+                content.setTitle(newContentInfo.getTitle());
+                return cmsRepository.save(content);
+            }
+            else 
+                return null;
+    }
 
  
-//     @Override
-//     public void deleteContent(int id){
-//         Optional<Content> contentEntity = cms.findById(id);
-//         if (!contentEntity.isPresent()) {
-//             throw new ContentNotFoundException(id);
-//         } else {
-//             cms.deleteById(id);
-//         }
-//     }
+    @Override
+    public void deleteContent(int id){
+        Optional<Content> contentEntity = cmsRepository.findById(id);
+        if (!contentEntity.isPresent()) {
+            throw new ContentNotFoundException(id);
+        } else {
+            cmsRepository.deleteById(id);
+        }
+    }
 
+    @Override
+    public Content isApproved(int id, boolean approvalValue){
+        Optional<Content> contentEntity = cmsRepository.findById(id);
+        Content content;
+        if (contentEntity.isPresent()) {
+            content = contentEntity.get();
+            content.setApproved(approvalValue);
+            return content;
+        } else 
+            return null;
 
-// }  
+    }
+
+    
+}
+  
