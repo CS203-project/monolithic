078482a5566c50c86d93fc6e1515e46dd9c3e3e9
Jonathan Chow <jonathanchowjh@gmail.com>
2020-11-02 01:59:23 +0800

    user

--- src/main/java/com/example/demo/user/User.java
+++ src/main/java/com/example/demo/user/User.java
@@ -95,7 +95,7 @@ public class User implements UserDetails {
   public String getPassword() { return password; }
   public void setPassword(String password) { this.password = password; }
 
-  // public String getAuthorities() { return authorities; }
+  public String getStrAuthorities() { return authorities; }
   public void setAuthorities(String authorities) { this.authorities = authorities; }
 
   public Boolean getActive() { return active; }
