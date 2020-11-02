99fc00a3e86cc4424397c3eb789336b258d5232e
Lindy Lim <lindylimlw@gmail.com>
2020-11-02 15:03:07 +0800

    Added details to MarketMaker

--- src/main/java/com/example/demo/trades/MarketMaker.java
+++ src/main/java/com/example/demo/trades/MarketMaker.java
@@ -13,12 +13,33 @@ import java.time.ZoneId;
 @ConditionalOnProperty(name="scheduling.enabled", matchIfMissing=true)
 public class MarketMaker {
 
+    /*
+Market Maker function: your API should be generating various sell and buy orders
+            for all traded stocks at appropriate bids and asks (based on actual last_price).
+            The volumes for these orders should be specified or fixed by your API, i.e.,
+            you can specify any volumes required for testing purposes, e.g., 20000.
+            This is to create liquidity in the market, and facilitate fast order matching.
+     
+        *** Example:
+        *** Your API can obtain initial static pricing from https://www.sgx.com/indices/products/sti
+        *** When your API starts (or market starts), your API will auto-create multiple open buy and sell trades,
+        *** one pair (buy and sell) for each stock listed at the bid and ask price, respectively.
+        *** The volumes of these trades can be set to a fixed value, say 20000.
+
+        *** E.g., for A17U, based on the actual last_price of $3.28, you can create a sell trade with ask = $3.29 & volume = 20000,
+        *** and a buy trade with bid = $3.26 & volume = 20000.
+        
+        *** These trades are referred to as the market maker's trades - to create liquidity in the market.
+        *** The customers' trades can then be matched with these market maker's trades.
+*/
+
     private Instant timestamp = Instant.now();
 
     public MarketMaker() {}
 
     // For testing: @Scheduled(cron="* * * * * *")
     @Scheduled(cron="* 0 0/1 * * MON-FRI")
+    // At minute 0 past every hour from 0 through 23 on every day-of-week from Monday through Friday.
     public void updateEveryHour() {
         timestamp = Instant.now();
     }
